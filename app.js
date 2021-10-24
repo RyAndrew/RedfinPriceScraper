d864debc744bda943048bcde7c0c37648b3a09b9
Andrew Ry <RyAndrew@users.noreply.github.com>
2021-10-24 13:54:39 -0500

    use stealth, cookies use functions

--- app.js
+++ app.js
@@ -1,46 +1,26 @@
 'use strict';
 
-const puppeteer = require('puppeteer')
-const fs = require('fs').promises
-const cookieFile = 'cookies.json'
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
-var cookies = false;
+const fs = require('fs').promises;
 
 (async () => {
-
-  try{
-    cookies = await fs.readFile(cookieFile, "binary")
-  }catch(err){
-    console.log('error reading cookie file')
-  }
+  let cookieFile = 'cookies.json'
+  let url = 'https://www.redfin.com/CA/Beverly-Hills/1441-Angelo-Dr-90210/home/143179574'
 
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36');
+  //await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36');
 
-  if(cookies !== false){
-    if(cookies.length < 1){
-      console.log('empty cookies file')
-      cookies = false;
-    }else{
-      let storedCookies
-      try{
-        storedCookies = JSON.parse(cookies)
-      }catch(err){
-        console.log('empty or invalid cookies')
-        cookies = false;
-      }
-      page.setCookie(...storedCookies)
-      console.log('setting cookies')
-    }
-  }else{
-    console.log('no existing cookies')
-  }
+  readCookies(cookieFile, page)
 
-  await page.goto('https://www.redfin.com/CA/Beverly-Hills/1441-Angelo-Dr-90210/home/143179574')
+  await page.goto(url)
+  await page.waitForTimeout(3000);
 
-  saveCookies(await page.cookies())
+  writeCookies(cookieFile, await page.cookies())
 
   const price = await page.evaluate(() => {
 
@@ -52,6 +32,8 @@ var cookies = false;
     return price[0].innerHTML
   })
 
+  //await page.screenshot({ path: 'screenshot.png', fullPage: true });
+
   if(price === false){
     console.log('error!')
   }else{
@@ -61,16 +43,44 @@ var cookies = false;
   await browser.close()
 })()
 
-async function saveCookies(pageCookies){
-    if(pageCookies.length < 1){
-      return;
-    }
-    pageCookies.sort((a, b) => a.name.localeCompare(b.name));
-    let cookieString = JSON.stringify(pageCookies)
-    try{
-      await fs.writeFile(cookieFile, cookieString)
-    }catch(error){
-      console.log('error writing cookie file')
-    }
+async function writeCookies(cookieFile, pageCookies){
+  if(pageCookies.length < 1){
+    return;
+  }
+  pageCookies.sort((a, b) => a.name.localeCompare(b.name));
+  try{
+    await fs.writeFile(cookieFile, JSON.stringify(pageCookies))
+  }catch(error){
+    console.log('error writing cookie file')
+  }
+    
+}
+
+async function readCookies(cookieFile, page){
+  let cookies = false;
+  try{
+    cookies = await fs.readFile(cookieFile, "binary")
+  }catch(err){
+    console.log('error reading cookie file')
+  }
+
+  if(cookies === false){
+    console.log('no existing cookies')
+    return
+  }
+  if(cookies.length < 1){
+    console.log('empty saved cookies file')
+    return
+  }
+
+  let storedCookies
+  try{
+    storedCookies = JSON.parse(cookies)
+  }catch(err){
+    console.log('empty or invalid saved cookies')
+    return
+  }
+  page.setCookie(...storedCookies)
+  console.log('using saved cookies')
     
 }
